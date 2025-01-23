917a855ccb318ae5c26bec4dfcdeb25cc665c4aa
Rafael Lawisch <lawisch.nh@gmail.com>
2025-01-22 21:00:15 -0300

    fix: obsolete reference to removed dependency

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,18 +1,5 @@
-
-import type { HandleClientError } from '@sveltejs/kit'
-// To use Clerk components:
-import { initializeClerkClient } from 'clerk-sveltekit/client'
-// Or for headless mode:
-// import { initializeClerkClient } from 'clerk-sveltekit/headless'
-import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public'
-
-initializeClerkClient(PUBLIC_CLERK_PUBLISHABLE_KEY, {
-	afterSignInUrl: '/admin/',
-	afterSignUpUrl: '/admin/',
-	signInUrl: '/sign-in',
-	signUpUrl: '/sign-up',
-})
+import type { HandleClientError } from "@sveltejs/kit";
 
 export const handleError: HandleClientError = async ({ error, event }) => {
-	console.error(error, event)
-}
+  console.error(error, event);
+};
