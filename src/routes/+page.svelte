21b4e55428d25a2ec20a7662d5b9824a48753c57
Lucca <luccajbecker@gmail.com>
2025-01-28 20:23:22 -0300

    style: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,20 @@
 {#if userId === undefined}
   <p>Loading...</p>
 {:else if userId === null}
-  <a href="/sign-in">Sign in</a> <span>|</span> <a href="/sign-up">Sign up</a>
+  <div class="flex justify-center">
+    <h1 class="text-2xl font-bold">Scouting - Under Control 1156</h1>
+  </div>
+  <div class="flex justify-center">
+    <div></div>
+    <div>
+      <div class="mx-4 flex-1 w-30 text-center rounded-full bg-sky-600 px-4 py-2  text-white transition hover:bg-sky-500 focus:outline-hidden focus:ring-2 focus:ring-sky-600 focus:ring-offset-2">
+        <a href="/sign-in">Sign in</a> 
+      </div>
+      <div class="mt-2  mx-4 flex-1 w-30 text-center rounded-full bg-sky-600 px-4 py-2 text-white transition hover:bg-sky-500 focus:outline-hidden focus:ring-2 focus:ring-sky-600 focus:ring-offset-2">
+        <a href="/sign-up">Sign up</a>
+      </div>
+    </div>
+  </div>
 {:else}
   <div class="ml-4 mt-4">
     <UserButton afterSignOutUrl="/" />
